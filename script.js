8505f293ee1efc1e88c03a2185c9282b5ff8c857
ShivanG <shivangramola007@gmail.com>
2023-12-03 10:41:17 +0530

    changes in code to work as expected

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
-const box1 = document.querySelector('.box1');
-const box2 = document.querySelector('.box2');
-const box3 = document.querySelector('.box3');
-const box4 = document.querySelector('.box4');
+const box1 = document.querySelector(".box1");
+const box2 = document.querySelector(".box2");
+const box3 = document.querySelector(".box3");
+const box4 = document.querySelector(".box4");
+const rest = document.querySelector(".reset");
+
 //EventListener click
-box1.addEventListener('click', function(){
-    console.log(box1.textContent);
+box1.addEventListener(
+  "click",
+  function () {
+    console.log("box 1 red is clicked");
+    document.body.style.background = "red";
+  },
+  false
+);
 
-}, false);
-box2.addEventListener('click', () =>{
-    console.log(box2.textContent);
-}, false)
-box3.addEventListener('click', () =>{
-    console.log(box3.textContent);
-}, false)
-box4.addEventListener('click', () =>{
-    console.log(box4.textContent);
-}, false)
+box2.addEventListener(
+  "click",
+  () => {
+    console.log("box 2 blue is clicked");
+    document.body.style.background = "blue";
+  },
+  false
+);
 
-box1.addEventListener('mouseover', function(){
-    document.body.style.backgroundColor = "pink";
-    
-})
+box3.addEventListener(
+  "click",
+  () => {
+    console.log("box 3 green is clicked");
+    document.body.style.background = "green";
+  },
+  false
+);
 
+box4.addEventListener(
+  "click",
+  () => {
+    console.log("box 4 yellow is clicked");
+    document.body.style.background = "yellow";
+  },
+  false
+);
 
+rest.addEventListener("click", () => {
+  console.log("rest is clicked");
+  document.body.style.backgroundColor = "#fffbf5";
+});
